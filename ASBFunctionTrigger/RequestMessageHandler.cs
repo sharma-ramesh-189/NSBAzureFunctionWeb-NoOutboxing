c062499963111659d967f34a9a0ae80d63c4684a
Ramesh Sharma <rsharma@STRASZ.COM>
2023-07-04 12:03:16 +0300

    Initial commit of Create a website and Azure function, both house NServicebus endpoints with no outboxing

---
+++ ASBFunctionTrigger/RequestMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using NServiceBus;
+using NServiceBus.Logging;
+using System.Threading.Tasks;
+
+namespace ASBFunctionTrigger
+{
+    public class RequestMessageHandler : IHandleMessages<RequestMessageCommand>
+    {
+        static readonly ILog Log = LogManager.GetLogger<RequestMessageHandler>();
+        public Task Handle(RequestMessageCommand message, IMessageHandlerContext context)
+        {
+            Log.Info($"Recieved Message: {message.Message}");
+            Log.Warn($"Handling {nameof(RequestMessageCommand)} in {nameof(RequestMessageHandler)}");
+            return context.Reply(new ResponseMessage { Message="This message is reply for request messsage."});
+        }
+    }
+}
---
+++ Domain/RequestMessageCommand.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+namespace Domain
+{
+    public class RequestMessageCommand:ICommand
+    {
+        
+        public string Message { get; set; }
+    }
+}
